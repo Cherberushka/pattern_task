90d949b21748fe47e950a428ed2e7636c6d85e17
Sergey Kluev <kluev.ekb9@gmail.com>
2024-01-25 18:28:44 +0500

    changes observer

--- src/factory/BasicCourseFactory.java
+++ src/factory/BasicCourseFactory.java
@@ -5,7 +5,6 @@ import portal.Course;
 public class BasicCourseFactory implements CourseFactory{
     @Override
     public Course createCourse(String courseName) {
-
         return new Course(courseName);
     }
 }
